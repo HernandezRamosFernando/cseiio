c4fddd007af55a20ba0ee9ce39b7e55c684e7b37
fer250795 <fernando.hernandez@itoaxaca.edu.mx>
2019-03-15 14:29:57 -0600

    funciona

--- application/models/M_aspirante.php
+++ application/models/M_aspirante.php
@@ -63,10 +63,6 @@ class M_aspirante extends CI_Model {
     $this->db->update('Lengua_materna', $datos_aspirante_lengua);
 
 
-    //actualizacion Tutor aspirante
-    $this->db->where('Aspirante_no_control', $no_control);
-    $this->db->update('Datos_Secundaria', $datos_aspirante_secundaria);
-
     //actualizacion datos medicos
     $this->db->where('Aspirante_no_control', $no_control);
     $this->db->update('Datos_medicos_aspirante', $datos_aspirante_medicos);
@@ -74,24 +70,26 @@ class M_aspirante extends CI_Model {
 
 if ($this->db->trans_status() === FALSE)
 {
-        return "algo salio mal";
+   ?>
+    <script>
+      alert("algo salio mal");
+      </script>
+   <?php
+        //return "alert(algo salio mal)";
 }
    
 else{
-   return "Registro actualizado correctamente";
+   ?>
+    <script>
+      alert("Registro actualizados correctamente");
+      </script>
+   <?php
 }
      
       
     
       
 
-      ?>
-   
-      <script>
-      alert("Registro actualizados correctamente");
-      </script>
-   
-      <?php
    }
 
 

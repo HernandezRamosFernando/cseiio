ee161144281d402dd9feef7e66f5d39d1356db59
HernandezRamosFernando <fernando.hernandez@itoaxaca.edu.mx>
2019-03-27 10:58:15 -0600

    headers 2

--- application/controllers/C_menu.php
+++ application/controllers/C_menu.php
@@ -1,9 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 class C_menu extends CI_Controller{
     public function inscripcion(){
-        $this->load->view("headers/cabezera");
+        $this->load->view("headers/cabecera");
         $this->load->view("admin/inscripcion");
     }
     
--- application/views/subirdocumentos/buscar_aspirante.php
+++ application/views/subirdocumentos/buscar_aspirante.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
 
   <title>Control Documentos</title>
-
-  <!-- Bootstrap core CSS-->
-  <link href="/cseiio/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template-->
-  <link href="/cseiio/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-  <!-- Custom styles for this template-->
-  <link href="/cseiio/assets/css/sb-admin.css" rel="stylesheet">
-  <link href="/cseiio/assets/vendor/bootstrap/css/bootstrap-float-label.css" rel="stylesheet">
-  <link href="/cseiio/assets/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-
-  <link rel="stylesheet" type="text/css" href="/cseiio/assets/css/main.css">
-
-
-
-
 </head>
 
 <body>

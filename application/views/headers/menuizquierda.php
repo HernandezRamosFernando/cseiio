e3322f33fd84bab64c1cd2fb7a55f7e74e1a3d71
SanchezHernandezChristian <sanchristian9@gmail.com>
2019-04-02 11:52:36 -0600

    menu deslizable

--- application/views/headers/menuizquierda.php
+++ application/views/headers/menuizquierda.php
@@ -9,32 +9,35 @@
     </li>
 
     <li class="nav-item dropdown ">
-      <a class="nav-link nav-dropdown-toggle  <?php ($this->uri->segment(2)=='inscripcion') ? print 'bg-info text-light' : print 'null'; ?>"
+      <a class="nav-link nav-dropdown-toggle  <?php ($this->uri->segment(2)=='inscripcion') ? print 'bg-info text-light' : print null; ?>"
       aria-expanded="false" data-toggle="collapse" href="#submenu" role="button">
         <i class="material-icons">group_add</i>
         <span class="font-weight-light">Inscripción<span>
       </a>
-      <div id="submenu" class="collapse sidebar-submenu">
-        <a class="nav-link btn-responsive  <?php ($this->uri->segment(2)=='nuevo_ingreso') ? print 'bg-info text-light' : print 'null'; ?>" href="/cseiio/index.php/c_aspirante/nuevo_ingreso">
+      <div id="submenu" class="<?php ($this->uri->segment(2)=='nuevo_ingreso'||$this->uri->segment(2)=='portabilidad'||$this->uri->segment(2)=='asignar_matricula'||$this->uri->segment(2)=='carta_compromiso') ? print 'null' : print 'collapse'; ?> sidebar-submenu">
+        <a class="nav-link <?php ($this->uri->segment(2)=='nuevo_ingreso') ? print 'bg-info text-light' : print null; ?>" href="/cseiio/index.php/c_aspirante/nuevo_ingreso">
         <i class="material-icons">person_add</i>
           <span class="font-weight-light">Inscripción Nuevo Ingreso
           </span>
         </a>
-        <a class="nav-link btn-responsive " href="/cseiio/index.php/c_aspirante/portabilidad">
+        <a class="nav-link  <?php ($this->uri->segment(2)=='portabilidad') ? print 'bg-info text-light' : print null; ?>" href="/cseiio/index.php/c_aspirante/portabilidad">
         <i class="material-icons">person_outline</i>
           <span class="font-weight-light">Inscripción Portabilidad
           </span>
         </a>
-        <a class="nav-link btn-responsive " href="/cseiio/index.php/c_aspirante/asignar_matricula">
+        <a class="nav-link  <?php ($this->uri->segment(2)=='asignar_matricula') ? print 'bg-info text-light' : print null; ?>" href="/cseiio/index.php/c_aspirante/asignar_matricula">
         <i class="material-icons">assignment_turned_in</i>
           <span class="font-weight-light">Asignar Matrícula</span>
         </a>
-        <a class="nav-link btn-responsive " href="/cseiio/index.php/c_aspirante/carta_compromiso">
+        <a class="nav-link   <?php ($this->uri->segment(2)=='carta_compromiso') ? print 'bg-info text-light' : print null; ?>" href="/cseiio/index.php/c_aspirante/carta_compromiso">
         <i class="material-icons">thumbs_up_down</i>
-          <span class="font-weight-light">Generación de Carta Compromiso</span>
+          <span class="font-weight-light">Carta Compromiso</span>
         </a>
+        <hr class="bg-info" style=" border: 3px solid ;">
       </div>
+      
     </li>
+    
     <li class="nav-item ">
       <a class="nav-link" href="/cseiio/index.php/c_menu/principal">
         <i class="material-icons">360person</i>

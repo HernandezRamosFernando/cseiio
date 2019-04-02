d389ad1133782fafa8c3481f0248090c62023196
SanchezHernandezChristian <sanchristian9@gmail.com>
2019-04-02 14:28:23 -0600

    cambios en vistas 6

--- application/views/plantel/subirdocumentos.php
+++ application/views/plantel/subirdocumentos.php
@@ -39,7 +39,6 @@
                   <label class="form-group has-float-label">
                     <select class="form-control form-control-lg" required="required" id="aspirante_plantel_busqueda"
                       name="aspirante_plantel">
-                      <option value="">Buscar en todos los planteles</option>
 
                       <?php
               foreach ($planteles as $plantel)

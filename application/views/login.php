7fc9b041ce594973b002701cb0b4e5210ef0e24f
SanchezHernandezChristian <sanchristian9@gmail.com>
2019-04-02 13:12:11 -0600

    usuarios 1

--- application/views/login.php
+++ application/views/login.php
@@ -25,14 +25,35 @@
             </div>
           </div>
   
-          <button type="submit">Ingresar</button>
+          <button class="btn btn-success btn-block btn-lg" type="submit">Ingresar</button>
         </form>
+        <hr>
 
 
 
         <div class="text-center">
-          <a class="d-block small" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
+          <a class="d-block small"  data-toggle="modal" data-target="#modalcontraseña">¿Olvidaste tu contraseña?</a>
         </div>
       </div>
     </div>
   </div>
+
+  
+  <!-- Modal -->
+  <div class="modal fade" id="modalcontraseña" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content card">
+              <div class="modal-body">
+                  <div class="container-fluid">
+                      Favor de llamar al numero siguiente:
+                      <br>
+                      O enviar mensaje al siguiente correo:
+                  </div>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-warning" data-dismiss="modal">Cerrar</button>
+              </div>
+          </div>
+      </div>
+  </div>
+  
---
+++ application/views/plantel/inscripcionplantel.php
@@ -0,0 +1,63 @@
+
+    <div id="content-wrapper">
+
+<div class="container-fluid ">
+
+  <!-- Breadcrumbs-->
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item">
+      <a>Inscripción</a>
+    </li>
+    <li class="breadcrumb-item active">Seleccione un elemento de los mostrados</li>
+  </ol>
+
+  <!-- Botones-->
+  <div class="row position-static" style="height: 350px">
+    <div class="col-md-4 col-lg-4">
+      <a href="<?php echo base_url();?>index.php/c_aspirante/nuevo_ingreso"
+        class="btn btn-primary btn-lg btn-block menu btn-1 btn-responsive">
+        <i class="material-icons">person_add</i>
+        Inscripción Nuevo Ingreso
+      </a>
+    </div>
+    <div class="col-md-4 col-lg-4">
+      <a href="<?php echo base_url();?>index.php/c_aspirante/portabilidad"
+        class="btn btn-primary btn-lg btn-block menu btn-2 btn-responsive">
+        <i class="material-icons">person_outline</i>
+        Inscripción Portabilidad
+      </a>
+    </div>
+    <div class="col-md-4 col-lg-4">
+      <a href="<?php echo base_url();?>index.php/c_aspirante/carta_compromiso"
+        class="btn btn-primary btn-lg btn-block menu btn-4 btn-responsive">
+        <i class="material-icons">thumbs_up_down</i>
+        Generación de Carta Compromiso
+      </a>
+    </div>
+    <!--
+    <div class="col col-lg-4">
+      <a href="<?php echo base_url();?>index.php/c_aspirante/carta_compromiso"
+        class="btn btn-primary btn-lg btn-block stretched-link center-block fas fa-clipboard-check btn-responsive"
+        style="height: 80%; background: #B7156D; border-color: #B7156D; padding: 13% ">
+        Crear ciclo escolar
+      </a>
+    </div>
+    <div class="col col-lg-4">
+      <a href="<?php echo base_url();?>index.php/c_aspirante/carta_compromiso"
+        class="btn btn-primary btn-lg btn-block stretched-link center-block fas fa-clipboard-check btn-responsive"
+        style="height: 80%; background: #B7156D; border-color: #B7156D; padding: 13% ">
+        Datos de secundaria
+      </a>
+    </div>
+    -->
+  </div>
+
+
+</div>
+</div>
+<!-- /.content-wrapper -->
+</div>
+<!-- /#wrapper -->
+
+
+

6a8886b67c117d2072987ecfd5757c048411347e
HernandezRamosFernando <fernando.hernandez@itoaxaca.edu.mx>
2019-05-07 12:25:18 -0500

    ciclos escolares

--- application/models/M_ciclo_escolar.php
+++ application/models/M_ciclo_escolar.php
@@ -10,7 +10,7 @@ class M_ciclo_escolar extends CI_Model {
    }
 
    public function get_datos_siguiente_ciclo(){
-      $registros_ciclo = $this->db->query("select count(*) as n from Ciclo_escolar where fecha_inicio=(select max(fecha_inicio) from Ciclo_escolar)")->result();
+      $registros_ciclo = $this->db->query("select count(*) as n from Ciclo_escolar where nombre_ciclo_escolar=(select nombre_ciclo_escolar from Ciclo_escolar where fecha_inicio= (select max(fecha_inicio) from Ciclo_escolar))")->result();
       if($registros_ciclo[0]->n<2){
          return $this->get_ciclo_escolar();
       }
@@ -19,4 +19,24 @@ class M_ciclo_escolar extends CI_Model {
          return array('respuesta'=>true);
       }
    }
+
+
+
+   public function agregar_ciclo_escolar($datos){
+      $this->db->trans_start();
+      $this->db->query("insert into Ciclo_escolar (fecha_matricula,nombre_ciclo_escolar,fecha_inicio,fecha_terminacion,periodo)
+      values (".$datos->fecha_matricula.",'".$datos->nombre_ciclo."','".$datos->fecha_inicio."','".$datos->fecha_terminacion."','".$datos->periodo."')");
+      $this->db->trans_complete();
+
+      if ($this->db->trans_status() === FALSE)
+      {
+            return "no";
+      }
+
+      else{
+         return "si";
+      }
+   }
+
+
 }

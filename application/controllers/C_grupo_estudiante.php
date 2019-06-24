225b207d53e18d8ff023e25c3ed947e36bb725e8
cseiio <desarrolladorcseiio@gmail.com>
2019-06-24 01:18:07 -0500

    Añadimos funciones para generar lista de asistencia

--- application/controllers/C_grupo_estudiante.php
+++ application/controllers/C_grupo_estudiante.php
@@ -8,6 +8,7 @@ class C_grupo_estudiante extends CI_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->model("M_grupo_estudiante");
+        $this->load->model("M_plantel");
     }
 
 
@@ -19,5 +20,31 @@ class C_grupo_estudiante extends CI_Controller {
 
     public function calificaciones_grupo_materia(){
         
+    }
+
+    //------------------------------------
+    public function generar_lista_asistencia(){
+    $id_materia = $this->input->post('materias');
+    $id_grupo = $this->input->post('grupos');
+    $mes = $this->input->post('mes');
+    $cct_plantel = $this->input->post('plantel');
+    $nombre_plantel=$this->M_plantel->get_plantel($cct_plantel)[0]->nombre_plantel;
+    $semestre_grupo = $this->input->post('semestre_grupo');
+    $datos_grupo=$this->M_grupo_estudiante->get_datos_grupo_estudiante_asesor($id_grupo,$id_materia);
+
+    $anio=date("Y");
+
+    //$nombre_grupo=$this->input->post('');
+
+     $datos['lista_alumnos']=$this->M_grupo_estudiante->nombres_estudiantes_grupo_materia($id_grupo,$id_materia);
+     $datos['datos_grupo']=$datos_grupo;
+     $datos['mes']=$mes;
+     $datos['anio']=$anio;
+     $datos['cct_plantel']=$cct_plantel;
+     $datos['nombre_plantel']=$nombre_plantel;
+     $datos['semestre']=$semestre_grupo;
+
+    $this->load->library('pdf');
+    $this->load->view('reportes/formato_asistencia',$datos);
     }
 }

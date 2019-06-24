73ca1fb658a46f6443807c9c5750319a625270b9
SanchezHernandezChristian <sanchristian9@gmail.com>
2019-06-24 10:29:34 -0500

    cambios plantel

--- application/views/plantel/inscripcionplantel.php
+++ application/views/plantel/inscripcionplantel.php
@@ -1,63 +1,87 @@
+<div id="content-wrapper">
 
-    <div id="content-wrapper">
+  <div class="container-fluid ">
 
-<div class="container-fluid ">
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a>Inscripción</a>
+      </li>
+      <li class="breadcrumb-item active">Seleccione un elemento de los mostrados</li>
+    </ol>
 
-  <!-- Breadcrumbs-->
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item">
-      <a>Inscripción</a>
-    </li>
-    <li class="breadcrumb-item active">Seleccione un elemento de los mostrados</li>
-  </ol>
+    <!-- Botones-->
+    <div class="row position-static" style="">
+      <div class="col-md-4 col-lg-4">
+        <a href="<?php echo base_url();?>index.php/c_vistas/nuevo_ingreso"
+          class="btn btn-primary btn-lg btn-block  btn-responsive menu  btn-1">
+          <div class="row">
+            <div class="col-2 valign-left">
+              <span style="font-size: 80px;  text-align: left">1</span>
+            </div>
+            <div class="col-4 valign-left" style="padding-left: 0px;">
+            &nbsp;<p class="h5"><strong>Inscripción Nuevo Ingreso</strong></p> &nbsp;
+            </div>
+            <div class="col-5 valign-right">
+              <i class="material-icons md-48">person_add</i>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-md-4 col-lg-4">
+        <a href="<?php echo base_url();?>index.php/c_vistas/portabilidad"
+          class="btn btn-primary btn-lg btn-block  btn-responsive menu  btn-2">
+          <div class="row">
+            <div class="col-2 valign-left">
+              <span style="font-size: 80px;  text-align: left">2</span>
+            </div>
+            <div class="col-4 valign-left" style="padding-left: 0px;">
+            &nbsp;<p class="h5"><strong>Inscripción Portabilidad</strong></p> &nbsp;
+            </div>
+            <div class="col-5 valign-right">
+              <img class="icono_menu" src="<?php echo base_url();?>assets/img/portabilidad.png">
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-md-4 col-lg-4">
+        <a href="<?php echo base_url();?>index.php/c_vistas/carta_compromiso"
+          class="btn btn-primary btn-lg btn-block  btn-responsive menu  btn-5">
+          <div class="row">
+            <div class="col-2 valign-left">
+              <span style="font-size: 80px;  text-align: left">5</span>
+            </div>
+            <div class="col-4 valign-left" style="padding-left: 0px;">
+            &nbsp;<p class="h5"><strong>Generación de Carta Compromiso</strong></p> &nbsp;
+            </div>
+            <div class="col-5 valign-right">
+              <img class="icono_menu" src="<?php echo base_url();?>assets/img/cartacompromiso.png">
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <!--
+          <div class="col col-lg-4">
+            <a href="<?php echo base_url();?>index.php/c_vistas/carta_compromiso"
+              class="btn btn-primary btn-lg btn-block stretched-link center-block fas fa-clipboard-check btn-responsive"
+              style="height: 80%; background: #B7156D; border-color: #B7156D; padding: 13% ">
+              Crear ciclo escolar
+            </a>
+          </div>
+          <div class="col col-lg-4">
+            <a href="<?php echo base_url();?>index.php/c_vistas/carta_compromiso"
+              class="btn btn-primary btn-lg btn-block stretched-link center-block fas fa-clipboard-check btn-responsive"
+              style="height: 80%; background: #B7156D; border-color: #B7156D; padding: 13% ">
+              Datos de secundaria
+            </a>
+          </div>
+          -->
+    </div>
 
-  <!-- Botones-->
-  <div class="row position-static" >
-    <div class="col-md-4 col-lg-4">
-      <a href="<?php echo base_url();?>index.php/c_aspirante/nuevo_ingreso"
-        class="btn btn-primary btn-lg btn-block menu valign-center btn-1 btn-responsive">
-        <i class="material-icons">person_add</i>&nbsp;
-        Inscripción Nuevo Ingreso
-      </a>
-    </div>
-    <div class="col-md-4 col-lg-4">
-      <a href="<?php echo base_url();?>index.php/c_aspirante/portabilidad"
-        class="btn btn-primary btn-lg btn-block menu valign-center btn-2 btn-responsive">
-        <i class="material-icons">person_outline</i>&nbsp;
-        Inscripción Portabilidad
-      </a>
-    </div>
-    <div class="col-md-4 col-lg-4">
-      <a href="<?php echo base_url();?>index.php/c_aspirante/carta_compromiso"
-        class="btn btn-primary btn-lg btn-block menu valign-center btn-4 btn-responsive">
-        <i class="material-icons">thumbs_up_down</i>&nbsp;
-        Generación de Carta Compromiso
-      </a>
-    </div>
-    <!--
-    <div class="col col-lg-4">
-      <a href="<?php echo base_url();?>index.php/c_aspirante/carta_compromiso"
-        class="btn btn-primary btn-lg btn-block stretched-link center-block fas fa-clipboard-check btn-responsive"
-        style="height: 80%; background: #B7156D; border-color: #B7156D; padding: 13% ">
-        Crear ciclo escolar
-      </a>
-    </div>
-    <div class="col col-lg-4">
-      <a href="<?php echo base_url();?>index.php/c_aspirante/carta_compromiso"
-        class="btn btn-primary btn-lg btn-block stretched-link center-block fas fa-clipboard-check btn-responsive"
-        style="height: 80%; background: #B7156D; border-color: #B7156D; padding: 13% ">
-        Datos de secundaria
-      </a>
-    </div>
-    -->
+
   </div>
-
-
-</div>
 </div>
 <!-- /.content-wrapper -->
 </div>
 <!-- /#wrapper -->
-
-
-

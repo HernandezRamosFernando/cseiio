e3d0901ef4739e0466e0060cbda1fc138968981f
SanchezHernandezChristian <sanchristian9@gmail.com>
2019-06-23 23:54:58 -0500

    notificaciones notificacion

--- application/views/admin/notificaciones.php
+++ application/views/admin/notificaciones.php
@@ -16,36 +16,37 @@
         <div class="form-group">
           <div class="row">
 
-              <div class="col-md-8" >
-                <label class="form-group has-float-label seltitulo">
-                  <select class="form-control form-control-lg selcolor" id="plantel_notificacion" name="plantel_notificacion">
-                    <option value="">Seleccione uno</option>
-                    <option value="todos">TODOS LOS USUARIOS DEL SISTEMA</option>
+            <div class="col-md-8">
+              <label class="form-group has-float-label seltitulo">
+                <select class="form-control form-control-lg selcolor" id="plantel_notificacion"
+                  name="plantel_notificacion">
+                  <option value="">Seleccione uno</option>
+                  <option value="todos">TODOS LOS USUARIOS DEL SISTEMA</option>
 
-                    <?php
+                  <?php
                                         foreach ($planteles as $plantel)
                                         {
                                           echo '<option value="'.$plantel->cct_plantel.'">'.$plantel->nombre_plantel.' ----- CCT: '.$plantel->cct_plantel.'</option>';
                                         }
                                         ?>
 
-                  </select>
-                  <span>Plantel al que desea enviar la notificación</span>
-                </label>
-              </div>
+                </select>
+                <span>Plantel al que desea enviar la notificación</span>
+              </label>
+            </div>
 
           </div>
         </div>
 
         <div class="form-group">
           <div class="row">
-          <div class="col-md-4">
-                <div class="form-label-group ">
-                  <input type="text" class="form-control text-uppercase" id="titulo_notificacion"
-                    placeholder="Título de la notificación">
-                  <label for="titulo_notificacion">Título de la notificación</label>
-                </div>
+            <div class="col-md-4">
+              <div class="form-label-group ">
+                <input type="text" class="form-control text-uppercase" id="titulo_notificacion"
+                  placeholder="Título de la notificación">
+                <label for="titulo_notificacion">Título de la notificación</label>
               </div>
+            </div>
 
 
           </div>
@@ -55,12 +56,12 @@
 
         <div class="form-group">
           <div class="row">
-          <div class="col-md-8">
-                <div class="form-group has-float-label seltitulo ">
-                  <textarea type="text" class="form-control text-uppercase" id="mensaje_notificacion"  rows="6"></textarea>
-                  <label for="mensaje_notificacion">Mensaje de la notificación</label>
-                </div>
+            <div class="col-md-8">
+              <div class="form-group has-float-label seltitulo ">
+                <textarea type="text" class="form-control text-uppercase" id="mensaje_notificacion" rows="6"></textarea>
+                <label for="mensaje_notificacion">Mensaje de la notificación</label>
               </div>
+            </div>
 
           </div>
         </div>
@@ -68,8 +69,8 @@
         <div class="form-group">
           <div class="row">
             <div class="col-md-4">
-              <button type="button" onclick="validarcomponente();" class="btn btn-warning btn-lg btn-block" style="padding: 1rem"
-                id="eliminar_usuario">Enviar notificación </button>
+              <button type="button" onclick="validarcomponente();" class="btn btn-warning btn-lg btn-block"
+                style="padding: 1rem" id="eliminar_usuario">Enviar notificación </button>
             </div>
           </div>
         </div>
@@ -86,7 +87,8 @@
 <!-- /#wrapper -->
 
 <!-- Modal -->
-<div class="modal fade" id="fechanotificacion" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+<div class="modal fade" id="fechanotificacion" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+  aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" style="max-width: 60% !important;" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -98,14 +100,14 @@
       <div class="modal-body">
 
 
-          <div class="col-md-12">
-            <div class="form-label-group">
-              <input type="date" class="form-control" id="fecha_fin" placeholder="Fecha de finalización " min=<?php
+        <div class="col-md-12">
+          <div class="form-label-group">
+            <input type="date" class="form-control" id="fecha_fin" placeholder="Fecha de finalización " min=<?php
                 echo date('Y-m-d');
                 ?>>
-              <label for="fecha_fin">Fecha de finalización de notificación </label>
-            </div>
+            <label for="fecha_fin">Fecha de finalización de notificación </label>
           </div>
+        </div>
 
 
       </div>
@@ -119,7 +121,7 @@
 
 
 <script>
-function validarcomponente() {
+  function validarcomponente() {
     if (document.getElementById("plantel_notificacion").value != '' && document.getElementById("titulo_notificacion").value != '' && document.getElementById("mensaje_notificacion").value != '') {
       $('#fechanotificacion').modal().show();
     } else {
@@ -129,7 +131,7 @@ function validarcomponente() {
       });
     }
   }
-function validarcomponentefecha() {
+  function validarcomponentefecha() {
     validafecha(document.getElementById("fecha_fin"));
 
     if (document.getElementById("fecha_fin").value != '') {
@@ -143,14 +145,14 @@ function validarcomponentefecha() {
   }
 
 
-  function mandar_notificacion(){
+  function mandar_notificacion() {
 
     let dato = {
-      plantel:document.getElementById("plantel_notificacion").value,
-      titulo:document.getElementById("titulo_notificacion").value,
-      mensaje:document.getElementById("mensaje_notificacion").value,
-      autor:"<?php echo $this->session->userdata('user')['usuario'] ?>",
-      fecha_fin:document.getElementById("fecha_fin").value
+      plantel: document.getElementById("plantel_notificacion").value,
+      titulo: document.getElementById("titulo_notificacion").value,
+      mensaje: document.getElementById("mensaje_notificacion").value,
+      autor: "<?php echo $this->session->userdata('user')['usuario'] ?>",
+      fecha_fin: document.getElementById("fecha_fin").value
     }
 
 
@@ -159,17 +161,38 @@ function validarcomponentefecha() {
 
     //Send the proper header information along with the request
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onloadstart = function(){
-        $('#div_carga').show();
-      }
-      xhr.error = function (){
-        console.log("error de conexion");
-      }
-  xhr.onreadystatechange = function () {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      $('#div_carga').hide();
-            console.log(xhr.response);
+    xhr.onloadstart = function () {
+      $('#div_carga').show();
+    }
+    xhr.error = function () {
+      console.log("error de conexion");
+    }
+    xhr.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        $('#div_carga').hide();
+        console.log(xhr.response);
+        if (xhr.responseText.trim() === "si") {
+          console.log(xhr.response);
+          swalWithBootstrapButtons.fire({
+            type: 'success',
+            text: 'Notificación enviada correctamente',
+            allowOutsideClick: false,
+            confirmButtonText: 'Aceptar'
+          }).then((result) => {
+            if (result.value) {
+              //aqui va el aceptar
+              $(document).scrollTop(0);
+              location.reload();
+            }
+            //aqui va si cancela
+          });
+        } else {
+          Swal.fire({
+            type: 'error',
+            text: 'Notificación no enviada'
+          });
         }
+      }
     }
     xhr.send(JSON.stringify(dato));
 
--- application/views/headers/cabecera.php
+++ application/views/headers/cabecera.php
@@ -1,72 +1,72 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta http-equiv="Content-Language" content="es">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <meta name="description" content="">
-            <meta name="author" content="">
-
-        
-            
-        
-            <!-- Bootstrap core CSS-->
-            <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-            <!-- Page level plugin CSS-->
-            <link href="<?php echo base_url();?>assets/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-            <!-- Custom styles for this template-->
-            <link href="<?php echo base_url();?>assets/css/sb-admin.css" rel="stylesheet">
-            <link href="<?php echo base_url();?>assets/css/main.css" rel="stylesheet" type="text/css" >
-            <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap-float-label.css" rel="stylesheet">
-            <!--<link href="<?php echo base_url();?>assets/vendor/material-design-icons/iconfont/material-icons.css" rel="stylesheet">-->
-            <script src="<?php echo base_url();?>assets/js/sweetalert2.all.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
-            <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
-            
-            <script src="<?php echo base_url();?>assets/vendor/jquery/jquery.min.js"></script>
-            <script src="<?php echo base_url();?>assets/js/funciones.js"></script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Language" content="es">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
 
-            <title><?=$title?></title>
 
-            <script>
-function cargar_notificaciones(){
-    var xhr = new XMLHttpRequest();
-        xhr.open('GET', '<?php echo base_url();?>index.php/c_notificacion/notificaciones_plantel?plantel=<?php echo $this->session->userdata('user')['plantel'] ?>', true);
-        xhr.onloadstart = function () {
-      $('#div_carga').show();
-    }
-    xhr.error = function () {
-      console.log("error de conexion");
-    }
-    xhr.onload = function () {
-      $('#div_carga').hide();
+
+  <!-- Bootstrap core CSS-->
+  <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Page level plugin CSS-->
+  <link href="<?php echo base_url();?>assets/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="<?php echo base_url();?>assets/css/sb-admin.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/css/main.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap-float-label.css" rel="stylesheet">
+  <!--<link href="<?php echo base_url();?>assets/vendor/material-design-icons/iconfont/material-icons.css" rel="stylesheet">-->
+  <script src="<?php echo base_url();?>assets/js/sweetalert2.all.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+  <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+
+  <script src="<?php echo base_url();?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/funciones.js"></script>
+
+
+  <title><?=$title?></title>
+
+  <script>
+    function cargar_notificaciones() {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', '<?php echo base_url();?>index.php/c_notificacion/notificaciones_plantel?plantel=<?php echo $this->session->userdata('user')['plantel'] ?>', true);
+      xhr.onloadstart = function () {
+        $('#div_carga').show();
+      }
+      xhr.error = function () {
+        console.log("error de conexion");
+      }
+      xhr.onload = function () {
+        $('#div_carga').hide();
         // aqui estan las notificaciones
         console.log(JSON.parse(xhr.response).length === 0);
         var a = "";
-        if(JSON.parse(xhr.response).length != 0){
-            $("#ic_notificacion").text('notifications_active');
-            $("#ic_notificacion").css( "color", "red" );
-        JSON.parse(xhr.response).forEach(function (valor, indice) {
-            a="<a class='dropdown-item'><span style= 'font-weight: bold'>" + valor.titulo + "</span><br> <span class='btn-responsive' >" + valor.mensaje +"</span><br> <span class='notificacion'>Enviado: " + valor.autor + "</span></a>";
+        if (JSON.parse(xhr.response).length != 0) {
+          $("#ic_notificacion").text('notifications_active');
+          $("#ic_notificacion").css("color", "red");
+          JSON.parse(xhr.response).forEach(function (valor, indice) {
+            a = "<a class='dropdown-item'><span style= 'font-weight: bold'>" + valor.titulo + "</span><br> <span class='btn-responsive' >" + valor.mensaje + "</span><br> <span class='notificacion'>Enviado: " + valor.autor + "</span></a>";
             $("#icononotificacion").append(a);
           });
-        }else{
-            a="<a class='dropdown-item'><span style= 'font-weight: bold'>No tiene notificaciones </span></a>";
-            $("#icononotificacion").append(a);
+        } else {
+          a = "<a class='dropdown-item'><span style= 'font-weight: bold'>No tiene notificaciones </span></a>";
+          $("#icononotificacion").append(a);
         }
-        }; 
-        xhr.send(null);
+      };
+      xhr.send(null);
 
-}
+    }
 
-$( document ).ready(function() {
-    cargar_notificaciones();
-});
-</script>
+    $(document).ready(function () {
+      cargar_notificaciones();
+    });
+  </script>
 
 
 </head>
-

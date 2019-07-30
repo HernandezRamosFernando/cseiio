2f9d80fc287ee7d37bd6f9a105e8c27e156d8769
cseiio <desarrolladorcseiio@gmail.com>
2019-07-30 01:09:52 -0500

    Cambios para formatos de baja

--- application/models/M_baja.php
+++ application/models/M_baja.php
@@ -18,7 +18,7 @@ class M_baja extends CI_Model {
 
 
    function lista_baja_estudiante_x_plantel_ciclo($plantel,$id_ciclo){
-      return $this->db->query("SELECT distinct ge.Estudiante_no_control,g.semestre,g.nombre_grupo,e.nombre,e.primer_apellido,e.segundo_apellido,b.motivo,b.fecha,b.observacion FROM Grupo_Estudiante ge inner join Grupo g on ge.Grupo_id_grupo=g.id_grupo inner join Baja b on b.Estudiante_no_control=ge.Estudiante_no_control inner join Estudiante e on e.no_control=ge.Estudiante_no_control where g.plantel='".$plantel."' and ge.Ciclo_escolar_id_ciclo_escolar=".$id_ciclo." order by g.semestre,g.nombre_grupo,e.primer_apellido,e.segundo_apellido,e.nombre;")->result();
+      return $this->db->query("SELECT distinct ge.Estudiante_no_control,g.semestre,g.nombre_grupo,e.nombre,e.primer_apellido,e.segundo_apellido,b.motivo,b.fecha,b.observacion FROM Grupo_Estudiante ge inner join Grupo g on ge.Grupo_id_grupo=g.id_grupo inner join Baja b on b.Estudiante_no_control=ge.Estudiante_no_control inner join Estudiante e on e.no_control=ge.Estudiante_no_control inner join Ciclo_escolar c on ge.Ciclo_escolar_id_ciclo_escolar=c.id_ciclo_escolar where g.plantel='".$plantel."' and c.id_ciclo_escolar=".$id_ciclo." and b.fecha between c.fecha_inicio_inscripcion and c.fecha_terminacion order by g.semestre,g.nombre_grupo,e.primer_apellido,e.segundo_apellido,e.nombre;")->result();
    }
 
    

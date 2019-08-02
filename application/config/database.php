b7791bfef687382af28d88f04372e777ff9dc559
cseiio <desarrolladorcseiio@gmail.com>
2019-08-02 12:49:38 -0500

    cambios de regularizacion

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.0.180',
-	'username' => 'admin',
-	'password' => 'admin',
+	'hostname' => '192.168.1.201',
+	'username' => 'root',
+	'password' => '',
 	//'hostname' => '127.0.0.1',
 	//'username' => 'root',
 	//'password' => 'admin',
